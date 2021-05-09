41cda6d67cf5815b53b595a641c4ec6de2c2ed73
Shobhit Jaiswal <38049846+ShobhitJaiswal@users.noreply.github.com>
2021-05-09 21:30:34 +0530

    Committing new file.

---
+++ oops/Constructor.py
@@ -0,0 +1,16 @@
+"""class Example:
+    def __new__(self):
+        return 'studytonight';
+
+# creating object of the class Example
+mutantObj = Example()
+
+# but this will return that our object 
+# is of type str
+print (type(mutantObj))"""
+
+class Example:
+    myVariable = "some value";
+
+simpleObj = Example()
+print (type(simpleObj))
